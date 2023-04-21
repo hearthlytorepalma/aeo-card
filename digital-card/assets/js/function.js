e6993041a467526e4217cfc29f873f724207c4b6
AXD <47520092+hearthlytorepalma@users.noreply.github.com>
2023-04-21 15:59:48 +0700

    Fixed undefined id

--- digital-card/assets/js/function.js
+++ digital-card/assets/js/function.js
@@ -1,15 +1,20 @@
 var saveBtn = document.getElementById("add-btn");
 saveBtn.addEventListener("click", function () {
   // Get the contact information from the website
+  try{gMobile = document.getElementById('mobile').href.split(':')[1]} catch(err){ gMobile = ''}
+  try{gWork = document.getElementById('work').href.split(':')[1]} catch(err) {gWork = ''}
+  try{gEmail = document.getElementById('email').href.split(':')[1]} catch(err) {gEmail = ''}
 
   var contact = {
     name: document.getElementById('name').innerHTML,
-    phone: document.getElementById('phone').href.split(':')[1],
-    email: document.getElementById('email').href.split(':')[1]
+    work: gWork,
+    mobile: gMobile,
+    email: gEmail
   };
 
+
   // create a vcard file
-  var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+  var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.work + "\nTEL;TYPE=mobile,voice:" + contact.mobile + "\nEMAIL:" + contact.email + "\nEND:VCARD";
   var blob = new Blob([vcard], { type: "text/vcard" });
   var url = URL.createObjectURL(blob);
   
